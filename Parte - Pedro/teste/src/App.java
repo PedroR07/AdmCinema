779a151905acd84f5d388d7cdc73c0d108e7506a
ParanoidAndroid <136632613+PedroR07@users.noreply.github.com>
2024-07-08 10:28:09 -0300

    Add files via upload

---
+++ Parte - Pedro/teste/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        // Cria e exibe a janela
+        MinhaTela tela = new MinhaTela();
+        tela.setVisible(true);
+    }
+}
