69c66f3ea6df12a44b2e3808aadc12546fd2ea61
ParanoidAndroid <136632613+PedroR07@users.noreply.github.com>
2024-08-12 10:49:05 -0300

    Delete Parte - Pedro/teste directory

--- Parte - Pedro/teste/src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        // Cria e exibe a janela
-        MinhaTela tela = new MinhaTela();
-        tela.setVisible(true);
-    }
-}
