f338d5285a4137f679bba5f6e035edbe48e4dd14
ParanoidAndroid <136632613+PedroR07@users.noreply.github.com>
2024-08-13 10:27:27 -0300

    Add files via upload

--- CineMais/src/CineMais/CatalogoFilmes.java
+++ CineMais/src/CineMais/CatalogoFilmes.java
@@ -48,40 +48,36 @@ public class CatalogoFilmes extends JFrame {
 
     private void loadFilmografia() {
         List<Filme> filmes = new ArrayList<>();
-
+    
         try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cinemais", "root", "");
              PreparedStatement stmt = conn.prepareStatement("SELECT * FROM filmografia");
-                ResultSet rs = stmt.executeQuery()) {
-
+             ResultSet rs = stmt.executeQuery()) {
+    
             while (rs.next()) {
+                int id = rs.getInt("ID");
                 String nome = rs.getString("Nome");
                 String diretor = rs.getString("Diretor");
-                // Remove acento circunflexo do Genero
                 String genero = rs.getString("Genero").replaceAll("\\^", "");
                 String ano = rs.getString("Ano");
                 String sinopse = rs.getString("Sinopse");
-
-                Filme filme = new Filme(nome, diretor, genero, ano, sinopse);
+    
+                Filme filme = new Filme(id, nome, diretor, genero, ano, sinopse);
                 filmes.add(filme);
             }
-
-            // Limpa o painel antes de adicionar novos botões
+    
             panel.removeAll();
-            int i = 0;
             for (Filme filme : filmes) {
-                i++;
-                JButton button = new JButton(); // Texto do botão é deixado em branco
-
-                // Adiciona uma imagem de fundo ao botão
-                ImageIcon originalIcon = new ImageIcon("C:/Users/2023101202010006/Documents/NetBeansProjects/CineMais/imagens/" + i + ".png"); // Substitua pelo caminho da sua imagem
+                JButton button = new JButton();
+                // Use o ID do filme para carregar a imagem correspondente
+                ImageIcon originalIcon = new ImageIcon("C:/Users/2023101202010006/Documents/NetBeansProjects/CineMais/imagens/" + filme.getId() + ".png");
                 Image img = originalIcon.getImage();
-                Image scaledImg = img.getScaledInstance(210, 320, Image.SCALE_SMOOTH); // Redimensiona a imagem
+                Image scaledImg = img.getScaledInstance(210, 320, Image.SCALE_SMOOTH);
                 ImageIcon scaledIcon = new ImageIcon(scaledImg);
                 button.setIcon(scaledIcon);
-
-                button.setPreferredSize(new Dimension(220, 330)); // Define o tamanho do botão para combinar com a imagem
-                button.setMargin(new Insets(0, 0, 0, 0)); // Remove margens ao redor do texto
-
+    
+                button.setPreferredSize(new Dimension(220, 330));
+                button.setMargin(new Insets(0, 0, 0, 0));
+    
                 button.addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
@@ -93,18 +89,17 @@ public class CatalogoFilmes extends JFrame {
                                 "Sinopse: " + filme.getSinopse());
                     }
                 });
-                
+    
                 panel.add(button);
             }
-
-            // Atualiza o painel após adicionar todos os botões
+    
             panel.revalidate();
             panel.repaint();
-
+    
         } catch (SQLException e) {
             e.printStackTrace();
             JOptionPane.showMessageDialog(null, "Erro ao conectar ao banco de dados: " + e.getMessage(),
-                                        "Erro", JOptionPane.ERROR_MESSAGE);
+                    "Erro", JOptionPane.ERROR_MESSAGE);
         }
     }
 
--- CineMais/src/CineMais/Filme.java
+++ CineMais/src/CineMais/Filme.java
@@ -1,39 +1,43 @@
 package CineMais;
+public class Filme {
+    private int id;
+    private String nome;
+    private String diretor;
+    private String genero;
+    private String ano;
+    private String sinopse;
 
-    public class Filme {
-        private String nome;
-        private String diretor;
-        private String genero;
-        private String ano;
-        private String sinopse;
-    
-        public Filme(String nome, String diretor, String genero, String ano, String sinopse) {
-            this.nome = nome;
-            this.diretor = diretor;
-            this.genero = genero;
-            this.ano = ano;
-            this.sinopse = sinopse;
-        }
-    
-        public String getNome() {
-            return nome;
-        }
-    
-        public String getDiretor() {
-            return diretor;
-        }
-    
-        public String getGenero() {
-            return genero;
-        }
-    
-        public String getAno() {
-            return ano;
-        }
-    
-        public String getSinopse() {
-            return sinopse;
-        }
+    public Filme(int id, String nome, String diretor, String genero, String ano, String sinopse) {
+        this.id = id;
+        this.nome = nome;
+        this.diretor = diretor;
+        this.genero = genero;
+        this.ano = ano;
+        this.sinopse = sinopse;
     }
-    
 
+    // Getters e setters para todos os campos, incluindo o ID
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+}
