37fa7c0b63bed1b8070f7446444fc97f0ef74507
ParanoidAndroid <136632613+PedroR07@users.noreply.github.com>
2024-08-13 09:41:07 -0300

    Delete CineMais directory

--- CineMais/src/CineMais/Main.java
+++
@@ -1,8 +0,0 @@
-package CineMais;
-
-public class Main {
-    public static void main(String[] args) {
-        TelaInicial telaInicial = new TelaInicial();
-        telaInicial.setVisible(true);
-    }
-}
